d0a73aa07b2de0d266b65ec9a8fa3645d989bd8d
tomazcarcamo1 <tomazcarcamo1@gmail.com>
2026-06-09 18:52:33 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+[vehiculos, setVehiculos] = useState([]) 
 
 function App() {}
 
